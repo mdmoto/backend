e144d7f3e7ecc6a8606a10facaf2ed951c2668f9
Adam Maollar <adam@maollar.com>
2026-03-23 09:58:41 +0900

    fix(controller): add legacy /buyer mapping to MaollarController

--- buyer-api/src/main/java/cn/lili/controller/other/MaollarController.java
+++ buyer-api/src/main/java/cn/lili/controller/other/MaollarController.java
@@ -35,7 +35,7 @@ import java.util.Map;
 @Slf4j
 @RestController
 @Api(tags = "Maollar DApp 集成接口")
-@RequestMapping({ "/api/v1/maollar" })
+@RequestMapping({ "/api/v1/maollar", "/buyer/maollar" })
 public class MaollarController {
 
     @org.springframework.beans.factory.annotation.Value("${lili.maollar.solana.gateway-secret:}")
